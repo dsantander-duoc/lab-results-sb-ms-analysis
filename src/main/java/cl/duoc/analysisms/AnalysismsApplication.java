61ea24df51f49d3ec1e3735af00a477a78709241
Daniel Santander <santanderad00@gmail.com>
2025-11-02 21:19:15 -0300

    first commit and project intilization

---
+++ src/main/java/cl/duoc/analysisms/AnalysismsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.analysisms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysismsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysismsApplication.class, args);
+	}
+
+}
